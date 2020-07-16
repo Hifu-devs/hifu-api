32febc53d2ebf317e305a574f766db1999b52dee
Margo Flewelling <flewelling.margo@gmail.com>
2020-07-16 15:48:50 -0600

    Add enum status, thinking we probably don't need it if we destroy the record

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,4 +3,7 @@ class Route < ApplicationRecord
   has_many :waypoints
   has_one :contact, through: :user
 
+# default status is 0, active
+# maybe we don't even need this if we are going to destroy upon checking in
+  enum status: {active: 0, complete: 1}
 end
