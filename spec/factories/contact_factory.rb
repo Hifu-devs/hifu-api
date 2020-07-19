1c7f1ebff5d8637a1d17bd48220cb49f2c65d154
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-19 09:33:57 -0600

    Update contact factory bad faker call

--- spec/factories/contact_factory.rb
+++ spec/factories/contact_factory.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :contact do
     name {Faker::Name.name}
     email {Faker::Internet.email}
-    phone {Faker:PhoneNumber.cell_phone}
+    phone {Faker::PhoneNumber.cell_phone}
   end
 end
