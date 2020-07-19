a91e4f80b34f7919545d1e0d55fe2af9c9357620
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-18 21:32:40 -0600

    Add waypoint self reference expectations

--- spec/models/waypoint_spec.rb
+++ spec/models/waypoint_spec.rb
@@ -3,5 +3,8 @@ require 'rails_helper'
 RSpec.describe Waypoint, type: :model do
   describe 'relationships' do
     it {should belong_to(:route)}
+    it {should have_one(:next)}
+
+    it {should belong_to(:previous).optional(true)}
   end
 end
