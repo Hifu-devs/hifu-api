59e35037542c379d6e5e83e63ee1709371e58041
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-18 23:40:00 -0600

    Update waypoint factory with eta column name

--- spec/factories/waypoint_factory.rb
+++ spec/factories/waypoint_factory.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
   factory :waypoint do
-    previous_wp {}
-    next_wp {}
     latitude {Faker::Address.latitude}
     longitude {Faker::Address.longitude}
-    estimated_eta {}
+    eta {}
   end
 end
