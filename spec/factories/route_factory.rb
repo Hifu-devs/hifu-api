d377792e88c9093fcbfd12b46603de7eabd8654e
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-21 17:31:54 -0600

    Update factory time with usec rounding

--- spec/factories/route_factory.rb
+++ spec/factories/route_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :route do
-    start_time {Faker::Time.between(from: 1.minutes.from_now, to: 1.days.from_now)}
-    end_time {Faker::Time.between(from: 1.days.from_now, to: 10.days.from_now)}
+    start_time {Faker::Time.between(from: 1.minutes.from_now, to: 1.days.from_now).round}
+    end_time {Faker::Time.between(from: 1.days.from_now, to: 10.days.from_now).round}
     activity {Faker::Verb.ing_form }
     party_size {Faker::Number.number(digits: 1)}
     notes {Faker::Music::Prince.lyric}
--- spec/factories/waypoint_factory.rb
+++ spec/factories/waypoint_factory.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :waypoint do
     latitude {Faker::Address.latitude}
     longitude {Faker::Address.longitude}
+    eta {Faker::Time.between(from: 1.minutes.from_now, to: 1.days.from_now).round}
   end
 end
