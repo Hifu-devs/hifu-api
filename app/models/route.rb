8903714201ad0dc4af32c1b1f4afa17385cefcd4
Margo Flewelling <flewelling.margo@gmail.com>
2020-07-22 16:24:48 -0600

    Add sending email method, broke email and sms into helper methods. Added Array(routes) to account for single route cases

--- app/models/route.rb
+++ app/models/route.rb
@@ -12,7 +12,13 @@ class Route < ApplicationRecord
   def self.send_alerts(time)
     time = time
     routes = Route.where(end_time: time)
-    routes.each do |route|
+    # send_text(routes)
+    send_email(routes)
+    routes
+  end
+
+  def self.send_text(routes)
+    Array(routes).each do |route|
       @phone = route.contact.phone.to_s
       @name = route.user.name.to_s
       @activity = route.activity.to_s
@@ -28,4 +34,39 @@ class Route < ApplicationRecord
       Faraday.post('https://hifu-sms.herokuapp.com/alert', json)
     end
   end
+
+  def self.send_email(routes)
+    Array(routes).each do |route|
+      info = { "data": {
+               "user": { "name": route.user.name,
+                         "email": route.user.email,
+                         "phone": route.user.phone,
+                         "address": route.user.address,
+                         "age": route.user.age,
+                         "race": route.user.race,
+                         "gender": route.user.gender,
+                         "sat_tracker_address": route.user.sat_tracker_address,
+                         "blood_type": route.user.blood_type,
+                         "allerges": route.user.allergies,
+                         "medical_conditions": route.user.medical_conditions,
+                         "heightCM": route.user.heightCM,
+                         "weightKG": route.user.weightKG,
+                         "contact": { "name": route.contact.name,
+                                    "email": route.contact.email,
+                                    "phone": route.contact.phone },
+                         "route": { "start_time": route.start_time,
+                                  "end_time": route.end_time,
+                                  "activity": route.activity,
+                                  "party_size": route.party_size,
+                                  "notes": route.notes,
+                                  "waypoints": route.waypoints }
+                          }
+                        }
+
+                  }
+      json = info.to_json
+      Faraday.post('https://nameless-ravine-82701.herokuapp.com/email_alert', json)
+    end
+  end
+
 end
