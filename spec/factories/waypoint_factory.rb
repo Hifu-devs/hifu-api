fd0b57d671dbdcfea84820fb604cdb38cc47830b
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-22 09:30:52 -0600

    Update waypoint factory round lat/long to 6 places

--- spec/factories/waypoint_factory.rb
+++ spec/factories/waypoint_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :waypoint do
-    latitude {Faker::Address.latitude}
-    longitude {Faker::Address.longitude}
+    latitude {Faker::Address.latitude.round(6)}
+    longitude {Faker::Address.longitude.round(6)}
     eta {Faker::Time.between(from: 1.minutes.from_now, to: 1.days.from_now).round}
   end
 end
