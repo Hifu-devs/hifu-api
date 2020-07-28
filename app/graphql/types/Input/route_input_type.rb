607d1bc282e41f32ddb78f4fbdbfac070de33a02
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-28 10:22:34 -0600

    Dates to DateTime

--- app/graphql/types/Input/route_input_type.rb
+++ app/graphql/types/Input/route_input_type.rb
@@ -2,8 +2,8 @@ module Types
   class Input::RouteInputType < Input::BaseInput
     description "Attributes for creating or updating a route"
     argument :userEmail, String, required: false
-    argument :startTime, Date, required: false
-    argument :endTime, Date, required: true
+    argument :startTime, DateTime, required: false
+    argument :endTime, DateTime, required: true
     argument :activity, String, required: false
     argument :partySize, Integer, required: false
     argument :notes, String, required: false
