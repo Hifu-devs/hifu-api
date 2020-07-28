aff5856cbecb9b8233395d11a892b1b4b6d92ea0
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-28 11:00:24 -0600

    Dates to isoDates

--- app/graphql/types/Input/route_input_type.rb
+++ app/graphql/types/Input/route_input_type.rb
@@ -2,8 +2,8 @@ module Types
   class Input::RouteInputType < Input::BaseInput
     description "Attributes for creating or updating a route"
     argument :userEmail, String, required: false
-    argument :startDate, Date, required: false
-    argument :endDate, Date, required: true
+    argument :startDate,  GraphQL::Types::ISO8601DateTime, required: false
+    argument :endDate,  GraphQL::Types::ISO8601DateTime, required: true
     argument :activity, String, required: false
     argument :partySize, Integer, required: false
     argument :notes, String, required: false
